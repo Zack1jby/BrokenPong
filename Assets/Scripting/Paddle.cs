b0744bb358d07c096420db8db18c67663ecc11c6
Isaiah Williams <isaiah.williams5711@gmail.com>
2025-07-17 14:36:43 -0500

    Adjusted variables. - Changed single line PlayerType to multiple line declaration - Changed playerType to CurrentPlayer - Changed s to Speed - Changed d to Boundary - Changed ia to movementInput - Changed m to movementDirection

--- Assets/Scripting/Paddle.cs
+++ Assets/Scripting/Paddle.cs
@@ -2,49 +2,52 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public enum PlayerType { Player1, Player2 }
+public enum PlayerType
+{
+    Player1, Player2
+}
 
 public class Paddle : MonoBehaviour
 {
-    public PlayerType playerType;
-    public float s = 10f;
-    public float d = 4f;
+    public PlayerType CurrentPlayer;
+    public float Speed = 10f;
+    public float Boundary = 4f;
 
-    [SerializeField] private InputAction ia;
-    private float m;
+    [SerializeField] private InputAction movementInput;
+    private float movementDirection;
 
     void Awake()
     {
-        ia.performed += OnMovePerformed;
-        ia.canceled += OnMoveCanceled;
+        movementInput.performed += OnMovePerformed;
+        movementInput.canceled += OnMoveCanceled;
     }
 
     void OnEnable()
     {
-        ia.Enable();
+        movementInput.Enable();
     }
     
     void OnDisable()
     {
-        ia.Disable();
+        movementInput.Disable();
     }
 
     private void OnMovePerformed(InputAction.CallbackContext context)
     {
-        m = context.ReadValue<float> ();
+        movementDirection = context.ReadValue<float> ();
     }
 
     private void OnMoveCanceled(InputAction.CallbackContext context)
     {
-        m = 0;
+        movementDirection = 0;
     }
 
     void Update()
     {
-        float movement = m * s * Time.deltaTime;
+        float movement = movementDirection * Speed * Time.deltaTime;
         transform.Translate(0f, movement, 0f);
 
-        float clampedY = Mathf.Clamp(transform.position.y, -d, d);
+        float clampedY = Mathf.Clamp(transform.position.y, -Boundary, Boundary);
         transform.position = new Vector3(transform.position.x, clampedY, 0f);
     }
 }
