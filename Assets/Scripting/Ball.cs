e8ad24356f16d876eff118ef1e8a95da4d274d98
Isaiah Williams <isaiah.williams5711@gmail.com>
2025-07-17 15:08:12 -0500

    Renamed variables for clarity - Renamed speed - Renamed direction - Renamed audioSource - Renamed clipBouncePlayer - Renamed clipBounceWall - Renamed clipScore

--- Assets/Scripting/Ball.cs
+++ Assets/Scripting/Ball.cs
@@ -3,30 +3,30 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    [SerializeField] private float x = 8f;
+    [SerializeField] private float speed = 8f;
     [SerializeField] private List<string> tags;
     [SerializeField] private string otherTag;
-    private Vector2 v;
+    private Vector2 direction;
 
-    [SerializeField] private AudioSource aS;
-    [SerializeField] private AudioClip clip1;
-    [SerializeField] private AudioClip clip2;
-    [SerializeField] private AudioClip clip3;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip clipBouncePlayer;
+    [SerializeField] private AudioClip clipBounceWall;
+    [SerializeField] private AudioClip clipScore;
     void Start()
     {
         transform.position = Vector2.zero;
-        v = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+        direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
     }
 
     void Update()
     {
-        transform.Translate(v * x * Time.deltaTime);
+        transform.Translate(direction * speed * Time.deltaTime);
     }
 
     private void ResetBall()
     {
         transform.position = Vector2.zero;
-        v = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+        direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -36,13 +36,13 @@ public class Ball : MonoBehaviour
         }
         else if (other.CompareTag(otherTag))
         {
-            v.y = -v.y;
+            direction.y = -direction.y;
         }
         else if (other.CompareTag("Player"))
         {
-            v.x = -v.x;
-            v.y = transform.position.y - other.transform.position.y;
-            v = v.normalized;
+            direction.x = -direction.x;
+            direction.y = transform.position.y - other.transform.position.y;
+            direction = direction.normalized;
         }
     }
 }
